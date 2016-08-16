c99994dd4e5a8f7a09d531ffdfb677c4905a8f6e
alianggitok <alianggmok@gmail.com>
2016-08-16 14:01:25 +0800

    tidy(del ngapp)

--- NGApp/view/partial/header/controller.js
+++
@@ -1,9 +0,0 @@
-define(['settings'],function(settings){
-	return ['$scope',function($scope){
-		
-		$scope.langs=settings.lang.langs;
-		$scope.appInfo=settings.info;
-		$scope.navi=settings.navi;
-		
-	}];
-});
